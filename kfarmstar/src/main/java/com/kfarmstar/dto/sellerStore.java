4fd9c0b6f70f0a4dc2cd39582d8b8a1a7b177db6
worbs1358@naver.com <worbs1358@naver.com>
2022-03-31 10:32:03 +0900

    [LJH] UPDATE

---
+++ kfarmstar/src/main/java/com/kfarmstar/dto/sellerStore.java
@@ -0,0 +1,115 @@
+package com.kfarmstar.dto;
+
+public class sellerStore {
+	private String sellerStoreNum;
+	private String memberId;
+	private String sellerStoreName;
+	private String sellerLicenseNum;
+	private String sellerStorePhone;
+	private String sellerEntryDate;
+	private String sellerStoreAddr;
+	private String sellerTaxType;
+	
+	private Member member;
+
+	public String getSellerStoreNum() {
+		return sellerStoreNum;
+	}
+
+	public void setSellerStoreNum(String sellerStoreNum) {
+		this.sellerStoreNum = sellerStoreNum;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getSellerStoreName() {
+		return sellerStoreName;
+	}
+
+	public void setSellerStoreName(String sellerStoreName) {
+		this.sellerStoreName = sellerStoreName;
+	}
+
+	public String getSellerLicenseNum() {
+		return sellerLicenseNum;
+	}
+
+	public void setSellerLicenseNum(String sellerLicenseNum) {
+		this.sellerLicenseNum = sellerLicenseNum;
+	}
+
+	public String getSellerStorePhone() {
+		return sellerStorePhone;
+	}
+
+	public void setSellerStorePhone(String sellerStorePhone) {
+		this.sellerStorePhone = sellerStorePhone;
+	}
+
+	public String getSellerEntryDate() {
+		return sellerEntryDate;
+	}
+
+	public void setSellerEntryDate(String sellerEntryDate) {
+		this.sellerEntryDate = sellerEntryDate;
+	}
+
+	public String getSellerStoreAddr() {
+		return sellerStoreAddr;
+	}
+
+	public void setSellerStoreAddr(String sellerStoreAddr) {
+		this.sellerStoreAddr = sellerStoreAddr;
+	}
+
+	public String getSellerTaxType() {
+		return sellerTaxType;
+	}
+
+	public void setSellerTaxType(String sellerTaxType) {
+		this.sellerTaxType = sellerTaxType;
+	}
+	
+	
+	
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("sellerStore [sellerStoreNum=");
+		builder.append(sellerStoreNum);
+		builder.append(", memberId=");
+		builder.append(memberId);
+		builder.append(", sellerStoreName=");
+		builder.append(sellerStoreName);
+		builder.append(", sellerLicenseNum=");
+		builder.append(sellerLicenseNum);
+		builder.append(", sellerStorePhone=");
+		builder.append(sellerStorePhone);
+		builder.append(", sellerEntryDate=");
+		builder.append(sellerEntryDate);
+		builder.append(", sellerStoreAddr=");
+		builder.append(sellerStoreAddr);
+		builder.append(", sellerTaxType=");
+		builder.append(sellerTaxType);
+		builder.append(", member=");
+		builder.append(member);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+}
